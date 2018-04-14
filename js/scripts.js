25277e27a9ba43e2199b02919c8ee4ec9e812da3
GG <gaiagonen@gmail.com>
2018-04-14 12:42:11 +0300

    docs: Organize js file docs

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ const timer = document.getElementById('timer');
 
 const frontSides = document.querySelectorAll('.card span');
 
-// Values that need to be matched. Cards.length should be equal to pairs.length
+// Values that need to be matched. cards.length should be equal to pairs.length
 let pairs = ["red", "purple", "green", "yellow", "orange", "pink", "deeppink", "lightblue",
 "red", "purple", "green", "yellow", "orange", "pink", "deeppink", "lightblue"];
 
@@ -22,6 +22,7 @@ let pairs = ["red", "purple", "green", "yellow", "orange", "pink", "deeppink", "
 //
 //---------------------------------------------------
 
+//A function to start the timer running
 function startTimer() {
   let secs = 0;
   let mins = 0;
@@ -61,6 +62,17 @@ function startTimer() {
   }, 1000);
 }
 
+// A function to randomize an array (changes the original array)
+// Implementaition of  Durstenfeld shuffle in EMACS6
+// Is introduced by Laurens Holst in Stack-over-flow
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
 
 
 
@@ -73,47 +85,19 @@ function startTimer() {
 //
 //---------------------------------------------------
 
-//TODO game functionality should begin only when pressing 'start game' or 'restart'.
-//consider changing the button's value instead of doing two buttons.
+// TODO game functionality should begin only when pressing 'start game' or 'restart'.
+// consider changing the button's value instead of doing two buttons.
+// The timer should also reset. HOW to make the functionality
 
+// A loop to add an event listner (click) on each card
 for (let card of cards) {
   card.addEventListener('click', function (event) {
     this.classList.toggle('back');
     this.classList.toggle('front');
   })
-} // A loop to add an event listner (click) on each card
-
-
-
-
-
-
-
-
-
-
-// Shuffle the deck
-
-
-// A function to randomize ann array (changes the original array)
-// Implementaition of  Durstenfeld shuffle in EMACS6
-// Is introduced by Laurens Holst in Stack-over-flow
-// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
-
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
 }
 
-shuffleArray(pairs);
-
-
+// A loop to set the changeable values for the cards after each shuffle
 for (let i=0; i < fronts.length; i++) {
   fronts[i].style.backgroundColor = pairs[i];
 }
-
-
-
-// TIMER
